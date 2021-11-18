7470461a0120f7abb6f93a0081f1c6a5b1db5846
KuzAnton8335 <ant2783@gmail.com>
2021-11-18 12:46:02 +0300

    slider-swiper

--- Blanchart__lp/js/script.js
+++ Blanchart__lp/js/script.js
@@ -44,3 +44,20 @@ modal.addEventListener('click', (event) => {
 closeBtn.onclick = function () {
 	modal.classList.add('hidden')
 }
+
+// slider
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	autoplay: {
+		delay: 3000,
+	},
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+});
